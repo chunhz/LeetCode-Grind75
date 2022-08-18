b4d2d8b4b5871acae7d58b4ba049d5fecfaa82ff
ChunHao Zheng <zheng@My-MacBook-Pro.lan>
2022-08-18 19:36:19 -0400

    finished 125

---
+++ Easy/121. Best Time to Buy and Sell Stock.js
@@ -0,0 +1,30 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ * Input: prices = [7,1,5,3,6,4]
+Output: 5
+Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
+ */
+ var maxProfit = function(prices) {
+  let minPrice = Number.MAX_VALUE;
+  let maxPrice = Number.MIN_VALUE;
+  let maxP = 0;
+  // let maxPrice
+    for(let i of prices){
+      maxPrice = Number.MIN_VALUE;
+      if(i < minPrice){
+        minPrice = i
+      }
+      if(i > maxPrice){
+        maxPrice = i;
+      }
+      if(maxPrice - minPrice > maxP)
+      maxP = maxPrice - minPrice
+    //  console.log('mix',maxPrice)
+    //  console.log('min',minPrice)
+    }
+    return maxPrice - minPrice;
+};
+console.log(maxProfit(
+  [7,6,4,3,1]))
--- Easy/125.Valid-Palindrome.js
+++ Easy/125.Valid-Palindrome.js
@@ -3,9 +3,10 @@
  * @return {boolean}
  */
 var isPalindrome = function(s) {
-    s = s.replace(/[^a-zA-Z]/gi, "").toLowerCase();
+    s = s.replace(/[^a-zA-Z0-9+]/gi, "").toLowerCase();
     let half = s.length/2
     console.log(s)
+    if(s.length === 1) return true
     for( let i = 0 ; i < half; i++){
         if(s[i] !== s[s.length-1-i])
         return false;
