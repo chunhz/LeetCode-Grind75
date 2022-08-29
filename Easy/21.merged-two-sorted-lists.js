740e7c30aae2de8041f6b6c1cf3bf294bf0f5286
ChunHao Zheng <zheng@My-MacBook-Pro.lan>
2022-08-28 23:13:15 -0400

    passed all tests

--- Easy/21.merged-two-sorted-lists.js
+++ Easy/21.merged-two-sorted-lists.js
@@ -14,31 +14,43 @@
        this.val = (val===undefined ? 0 : val)
        this.next = (next===undefined ? null : next)
    }
- var mergeTwoLists = function(list1, list2) {
-  // console.log(list1)
-  //  let ans;
-   const ans = new ListNode(-Infinity);
-   // a prev point to save the previously saved node
-   let prev = ans;
-   while (list1 && list2) {
-     // keep comparing list 1 and list 2, if list 1 is smaller, increment list 1 to list1.next.
-     if (list1.val <= list2.val) {
-      //  console.log(list1)
-       
-       prev.next = list1
-       prev = list1;
-       list1 = list1.next;
-     } else {
-      console.log(list2)
-       prev.next = list2
-       prev = list2
-       list2 = list2.next
-    }
-   }
-   if (!list1) prev.next = list2
-   if (!list2) prev.next = list1
-    // console.log(new ListNode(-Infinity))
-   return dummy.next;
+ /**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+
+var mergeTwoLists = function (list1, list2) {
+  // initiate a dummy list node
+  let dummy = new ListNode(0)
+  // copying the head of the dummy node
+  let dummyHead = dummy
+
+  
+  while (list1 != null && list2 != null) {
+    // comparing the values btw list1 & 2
+      if(list1.val < list2.val){
+        dummy.next = list1
+        //traversing list1
+        list1 = list1.next 
+      }
+      else {
+        dummy.next = list2
+        // traversing list2
+        list2 = list2.next
+      }
+    // dummy now pointing to the next element in dummy
+    dummy = dummy.next  
+  }   
+  //if ran out of list1, combine list 2 onto the dummy list
+  if(list1 == undefined)
+    dummy.next = list2
+  if(list2 == undefined)
+    dummy.next = list1
+  // return head.next will get rid of 0
+  return dummyHead.next;
 };
 let list1 = [1,2,4];
  let list2 = [1,3,4]
