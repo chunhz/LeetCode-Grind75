daeab575b4ba3850c549f83b25aa5b63d0388f5f
ChunHao Zheng <zheng@My-MacBook-Pro.lan>
2022-08-29 23:33:42 -0400

    passed all tests

---
+++ Easy/226.Invert-Binary-Tree.js
@@ -0,0 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+var invertTree = function (root) {
+	// if root is null return root
+	if (!root) return root;
+	const newTree = newTreeNode();
+
+	newTree.val = root.val;
+	// mirroring the tree, which left of the original tree will now be right
+	newTree.left = invertTree(root.right);
+	newTree.right = invertTree(root.left);
+	return newTree;
+};
