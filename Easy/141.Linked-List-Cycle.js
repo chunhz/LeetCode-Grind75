30ceb8bf71ac67f3176b7ea5a225a17e3ec0c0ba
chunhz <chz062009@gmail.com>
2022-09-05 10:21:59 -0400

    passed all tests

---
+++ Easy/141.Linked-List-Cycle.js
@@ -0,0 +1,40 @@
+/*141. Linked List Cycle
+
+Given head, the head of a linked list, determine if the linked list has a cycle in it.
+
+There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer. Internally, pos is used to denote the index of the node that tail's next pointer is connected to. Note that pos is not passed as a parameter.
+
+Return true if there is a cycle in the linked list. Otherwise, return false.
+
+Input: head = [3,2,0,-4], pos = 1
+Output: true
+Explanation: There is a cycle in the linked list, where the tail connects to the 1st node (0-indexed).
+*/
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var hasCycle = function (head) {
+	let slow = (fast = head);
+	while (fast && fast.next) {
+		slow = slow.next;
+		fast = fast.next.next;
+		if (slow == fast) return true;
+	}
+	return false;
+};
+// Input: head = [3,2,0,-4], pos = 1
+// Output: true
+// Explanation: There is a cycle in the linked list, where the tail connects to the 1st node (0-indexed).
+/*
+Runtime: 91 ms, faster than 76.23% of JavaScript online submissions for Linked List Cycle.
+Memory Usage: 44.8 MB, less than 76.70% of JavaScript online submissions for Linked List Cycle.
+*/
