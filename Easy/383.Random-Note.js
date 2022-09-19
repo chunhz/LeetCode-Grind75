3346a073a6c64d6613e383408bb0fbf809551d98
chunhz <chz062009@gmail.com>
2022-09-18 23:46:01 -0400

    passed all tests

---
+++ Easy/383.Random-Note.js
@@ -0,0 +1,43 @@
+/*383. Ransom Note
+
+Given two strings ransomNote and magazine, return true if ransomNote can be constructed by using the letters from magazine and false otherwise.
+
+Each letter in magazine can only be used once in ransomNote.
+
+ 
+
+Example 1:
+
+Input: ransomNote = "a", magazine = "b"
+Output: false
+Example 2:
+
+Input: ransomNote = "aa", magazine = "ab"
+Output: false
+Example 3:
+
+Input: ransomNote = "aa", magazine = "aab"
+Output: true
+*/
+/**
+ * @param {string} ransomNote
+ * @param {string} magazine
+ * @return {boolean}
+ */
+var canConstruct = function (ransomNote, magazine) {
+  let obj = {}
+  for (let i = 0; i < magazine.length; i++){
+     obj[magazine[i]] = (obj[i] || 0 ) + 1
+  }
+  for (let j = 0; j < ransomNote.length; j++){
+      console.log(obj)
+    if (!obj[ransomNote[j]]) return false;
+    obj[ransomNote[j]]--;
+  }
+ 
+
+  return true;
+ };
+const ransomNote = "aa", magazine = "aab"
+console.log(canConstruct(ransomNote, magazine))
+// Output: false
