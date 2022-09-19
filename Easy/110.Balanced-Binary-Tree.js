3a867583b3770a7355993dbc1f16ba740bdc7e17
chunhz <chz062009@gmail.com>
2022-09-18 23:12:03 -0400

    passed all tests

---
+++ Easy/110.Balanced-Binary-Tree.js
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+ var isBalanced = function(root) {
+  let ans = true
+  function dfs(root){
+      if(!root) return false
+      let left = dfs(root.left)
+      let right = dfs(root.right)
+      if(Math.abs(left-right)>1){
+          ans = false
+          return ;
+      }
+      return 1 + Math.max(left,right)
+  }
+  dfs(root)
+  return ans
+};
