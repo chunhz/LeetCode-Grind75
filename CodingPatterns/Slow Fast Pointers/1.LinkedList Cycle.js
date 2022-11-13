93e08e61d5b9c71c5c9290d4374bd055adf2e22f
chunhz <johnzheng9527@gmail.com>
2022-11-12 19:05:57 -0500

    passing all tests

--- CodingPatterns/Slow Fast Pointers/1.LinkedList Cycle.js
+++ CodingPatterns/Slow Fast Pointers/1.LinkedList Cycle.js
@@ -56,17 +56,83 @@ function has_cycle(head) {
 }
 // Time Comp: O(n), Space Comp: O(1)
 
+// const head = new Node(1);
+// head.next = new Node(2);
+// head.next.next = new Node(3);
+// head.next.next.next = new Node(4);
+// head.next.next.next.next = new Node(5);
+// head.next.next.next.next.next = new Node(6);
+// console.log(`LinkedList has cycle: ${has_cycle(head)}`);
+
+// head.next.next.next.next.next.next = head.next.next;
+// console.log(`LinkedList has cycle: ${has_cycle(head)}`);
+
+// head.next.next.next.next.next.next = head.next.next.next;
+// console.log(`LinkedList has cycle: ${has_cycle(head)}`);
+// console.log(`LinkedList has cycle: ${has_cycle(head)}`);
+
+/*
+Similar Problems
+
+Problem 1: Given the head of a LinkedList with a cycle, find the length of the cycle.
+
+Solution: We can use the above solution to find the cycle in the LinkedList. 
+Once the fast and slow pointers meet, we can save the slow pointer and iterate
+the whole cycle with another pointer until we see the slow pointer again to find the length of the cycle.
+*/
+
+function find_cycle_length(head) {
+  // let slow = head,
+  //   fast = head;
+  // while (fast !== null && fast.next !== null) {
+  //   fast = fast.next.next;
+  //   slow = slow.next;
+  //   if (slow === fast) {
+  //     // found the cycle
+  //     return calculate_cycle_length(slow);
+  //   }
+  // }
+  // return 0;
+  let slow = (fast = head);
+  while (fast && fast.next) {
+    fast = fast.next.next;
+    slow = slow.next;
+    if (slow === fast) {
+      console.log('found the cyclen');
+      return calculate_cycle_length(slow);
+    }
+  }
+}
+
+function calculate_cycle_length(slow) {
+  let curr = slow;
+  let cycLength = 0;
+  while (curr) {
+    curr = curr.next;
+    cycLength++;
+    if (curr === slow) break;
+  }
+  return cycLength;
+  // let current = slow,
+  //   cycle_length = 0;
+  // while (true) {
+  //   current = current.next;
+  //   cycle_length += 1;
+  //   if (current === slow) {
+  //     break;
+  //   }
+  // }
+  // return cycle_length;
+}
+
 const head = new Node(1);
 head.next = new Node(2);
 head.next.next = new Node(3);
 head.next.next.next = new Node(4);
 head.next.next.next.next = new Node(5);
 head.next.next.next.next.next = new Node(6);
-console.log(`LinkedList has cycle: ${has_cycle(head)}`);
-
 head.next.next.next.next.next.next = head.next.next;
-console.log(`LinkedList has cycle: ${has_cycle(head)}`);
+console.log(`LinkedList cycle length: ${find_cycle_length(head)}`);
 
 head.next.next.next.next.next.next = head.next.next.next;
-console.log(`LinkedList has cycle: ${has_cycle(head)}`);
-console.log(`LinkedList has cycle: ${has_cycle(head)}`);
+console.log(`LinkedList cycle length: ${find_cycle_length(head)}`);
